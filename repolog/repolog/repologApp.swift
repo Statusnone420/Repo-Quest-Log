26795377a20d52ad109fbd0ed1282da6f7394f67
Statusnone420 <anthonyga88@icloud.com>
2026-04-23 15:00:38 -0400

    Add initial repolog macOS SwiftUI project
    Create new macOS SwiftUI app 'repolog'. Adds generated Xcode project and workspace files, user scheme data, asset catalogs (AccentColor, AppIcon), and basic SwiftUI source files (repologApp.swift and ContentView.swift). Project metadata includes bundle identifier com.statusnone.repolog and development team CGX65757S6; build configurations and settings were generated by Xcode.

---
+++ repolog/repolog/repologApp.swift
@@ -0,0 +1,17 @@
+//
+//  repologApp.swift
+//  repolog
+//
+//  Created by Anthony Gagliardo on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct repologApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
